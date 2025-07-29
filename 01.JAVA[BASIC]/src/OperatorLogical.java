fa70fdd098d559f830983e26f77779641bebea6f
연승문 <yeonsm99@naver.com>
2025-07-29 11:18:46 +0900

    ...

---
+++ 01.JAVA[BASIC]/src/OperatorLogical.java
@@ -0,0 +1,18 @@
+
+public class OperatorLogical {
+
+	public static void main(String[] args) {
+		
+		/*
+		논리연산자
+		   - 형태:   ||(Logical OR) , && (Logical AND) 
+		   - 좌우측의항이 논리형변수(데이타)이다.
+           - 결과도 논리형데이타이다.
+		    ex> true || false, false && false
+        */
+		
+		
+
+	}
+
+}

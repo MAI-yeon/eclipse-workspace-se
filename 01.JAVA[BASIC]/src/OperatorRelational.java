0adf94c406398c05aeea83dee9d3f391e10c0402
연승문 <yeonsm99@naver.com>
2025-07-29 11:07:31 +0900

    ...

--- 01.JAVA[BASIC]/src/OperatorRelational.java
+++ 01.JAVA[BASIC]/src/OperatorRelational.java
@@ -2,8 +2,30 @@
 public class OperatorRelational {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		/*
+		 * 관계(비교)연산자
+		 *   - 형태: > , <, >= , <= , == ,!=
+		 *   - 관계연산의 결과값은 논리형데이타(true,false)
+		 */
+		
+		int a = 10;
+		int b = 20;
+		
+		boolean result = a>b;
+		System.out.println("10 > 20 -->" + result);
+        result = a < b;
+        System.out.println("10 < 20 -->" + result);
+        result = a <= b;
+        System.out.println("10 <= 20 -->" + result);
+        result = a >= b;
+        System.out.println("10 >= 20 -->" + result);
+        result = a == b;
+        System.out.println("10 == 20 -->" + result);
+        result = a != b;
+        System.out.println("10 != 20 -->" + result);
+        
+        
 	}
 
 }

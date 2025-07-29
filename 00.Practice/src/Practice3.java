b1f9313ee18d1a9b7eb489e1452e9129f8f099dd
연승문 <yeonsm99@naver.com>
2025-07-29 18:40:02 +0900

    ...

--- 00.Practice/src/Practice3.java
+++ 00.Practice/src/Practice3.java
@@ -18,14 +18,73 @@ public class Practice3 {
 		System.out.println(i5);
 
 		/*
-		 *  진수         이름          접두사          예시 
+		 * 진수 이름 접두사 예시
 		 * 
-		 * 2진수        Binary        0b 또는 0B     0b1010 → 10(십진수 10) 
-		 * 8진수        Octal         0o 또는 0O     0o12→ 10(십진수 10) 
-		 * 10진수       Decimal       없음           10 (일반 숫자) 
-		 * 16진수       Hexadecimal   0x 또는 0X     0xA →10(십진수 10)
+		 * 2진수 Binary 0b 또는 0B 0b1010 → 10(십진수 10) 8진수 Octal 0o 또는 0O 0o12→ 10(십진수 10)
+		 * 10진수 Decimal 없음 10 (일반 숫자) 16진수 Hexadecimal 0x 또는 0X 0xA →10(십진수 10)
 		 */
 
+		long l1 = 232;
+		long l2 = 2147483648L;
+		System.out.println(l1);
+		System.out.println(l2);
+
+		System.out.println("--------------double---------------");
+
+		float f1 = 0.1f;
+		System.out.println(f1);
+
+		double d1, d2, d3;
+		d1 = 3.14159;
+		d2 = 123456789123456789.0;
+		d3 = 1.23456789123456789;
+		System.out.println(d1);
+		System.out.println(d2);
+		System.out.println(d3);
+
+		System.out.println("--------------char----------------");
+
+		char c1;
+		c1 = 'A';
+		char c2 = '가';
+		char c3 = '힣';
+		char c4 = '1';
+		char c5 = c1;
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		System.out.println(c4);
+		System.out.println(c5);
+
+		char m1, m2, m3, m4, m5, m6;
+
+		m1 = 'A';
+		m2 = 65;
+		m3 = 0xAC00;
+		m4 = '\uAC00';
+		m5 = '가';
+		m6 = 44032;
+		System.out.println(m1);
+		System.out.println(m2);
+		System.out.println(m3);
+		System.out.println(m4);
+		System.out.println(m5);
+		System.out.println(m6);
+
+		/*
+		 * 65는 상수이기에 4바이트에서 2바이트로 잘릴때 데이터 소실이 없으면 기입 가능하다. 
+		 * "가" → U+AC00 (16진수) 
+		 *      = 0xAC00
+		 *      = 44032 (10진수)
+		 */
+
+		
+		int HangulCount = c3 - c2 + c1;
+		System.out.println("한글글자수 = "+ HangulCount);
+		
+		
+		
+		System.out.println("-----------------boolean-----------------");
 	}
 
 }

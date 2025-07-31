d75aa41401e91037cfc9426fc027754b196b551f
연승문 <yeonsm99@naver.com>
2025-07-31 13:39:56 +0900

    ...

--- 01.JAVA[BASIC]/src/ForTest.java
+++ 01.JAVA[BASIC]/src/ForTest.java
@@ -1,3 +1,5 @@
+import java.util.Iterator;
+
 public class ForTest {
 
 	public static void main(String[] args) {
@@ -39,8 +41,14 @@ public class ForTest {
 		System.out.println();
 		
 		System.out.println(">> 3과4의 공통배수만출력[1~100]");
+		for (int i = 1; i < 100; i++) {
+			if( i % 3 == 0 && i % 4 == 0) {
+				System.out.print(i+ " ");
+			}
 		
-	    
+		}
+		
+		System.out.println();
 		
 		
 		System.out.println(">> 3과4의 최소공배수만출력[1~100](break)");

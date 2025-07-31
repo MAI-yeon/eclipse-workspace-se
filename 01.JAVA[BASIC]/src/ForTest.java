98c78c2dc5ed0267bea641a2765d152e1ed62553
연승문 <yeonsm99@naver.com>
2025-07-31 13:45:31 +0900

    ....

--- 01.JAVA[BASIC]/src/ForTest.java
+++ 01.JAVA[BASIC]/src/ForTest.java
@@ -17,6 +17,7 @@ public class ForTest {
 			System.out.println("i = " + i);
 
 		}
+		
 		System.out.println("--------------for i 감소-------------");
 		for (int i = 9; i >= 0; i--) {
 			System.out.println("i = " + i);
@@ -28,6 +29,7 @@ public class ForTest {
 			if( i % 2 == 1)
 				System.out.print(i+" ");
 		}
+		
 		System.out.println();	
 		
 		System.out.println(">> 3의배수만출력[1~100]");
@@ -36,6 +38,7 @@ public class ForTest {
 			if(i % 3 == 0)
 				System.out.print(i + "");
 		}
+		
 		System.out.println();
 		
 		System.out.println(">> 3과4의 공통배수만출력[1~100]");
@@ -48,12 +51,15 @@ public class ForTest {
 		
 		System.out.println();
 		
-		
 		System.out.println(">> 3과4의 최소공배수만출력[1~100](break)");
+		
 		System.out.println(">> 4의 배수가출아닌수만출력[1~100](continue)");
 		System.out.println(">> 영문소문자(continue)");
 		System.out.println(">> 홀수,짝수합[1~100]");
 
+		
 	}
 
 }
+
+

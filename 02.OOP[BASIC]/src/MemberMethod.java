6066f7e7beeb9938d182c10446494ce43deec94b
연승문 <yeonsm99@naver.com>
2025-08-01 17:11:34 +0900

    ...

--- 02.OOP[BASIC]/src/MemberMethod.java
+++ 02.OOP[BASIC]/src/MemberMethod.java
@@ -13,5 +13,62 @@ public class MemberMethod {
 		System.out.println("\t method stmt1");
 		System.out.println("\t method stmt2");
 		System.out.println("\t void method1() 실행 끝");
+		
+		/*
+		 * << return >>
+		   - 모든 메쏘드블록의 마지막에는 항상 실행흐름을 호출한곳으로 반환하여야한다.
+		   - 실행흐름을 반환하는 문장이 return문이다.
+		   - return 문을 만나면 실행흐름이 호출한곳으로 반환된다.
+		   - return type이 void 인경우에는 생략이 가능하다.
+		   - return문이 생략된경우에는 컴파일러가 자동으로 return문을 삽입하여준다.
+		 */
+		
+		return;
+		
 	}
+	
+	/*
+	 * ☆☆☆매개변수(parameter=인자)가 있는 메소드 정의
+	 */
+	
+	void method2(int count) {
+		System.out.println("\t void method2(int count) 실행시작");
+		System.out.println("\t method2블록은 prameta로  선언된 로칼변수 : "+count);
+		int localVal=111;
+		System.out.println("\t method2 블록안에서 선언된 로칼변수 : "+localVal);
+		
+		for (int i = 0; i < count; i++) {
+			System.out.println("\t method stmt"+i);
+			
+		}
+		System.out.println("\t void method2(int count) 실행 끝 실행흐름 반환");
+		return;
+		
+		
+	}
+	
+	void method3(String msg, int count) {
+		System.out.println("void method3(String msg, int count) 실행시작" );
+		for (int i = 0; i < count; i++) {
+			System.out.println("\t\t"+msg);
+			
+		}
+		System.out.println("void method3(String msg, int count) 실행끝 실행흐름 반환");
+		return;
+		
+		
+	}
+	void methed4(String name, String msg, int count) {
+		System.out.println("\t 	void methed4(String name, String msg, int count)실행시작" );
+		for (int i = 0; i < count; i++) {
+			System.out.println("\t\t"+name+"님 "+msg);
+			
+		}
+		System.out.println("\t 	void methed4(String name, String msg, int count)) 실행끝 실행흐름 반환");
+		return;
+	}
+	
+	
+	
+	
 }
--- 02.OOP[BASIC]/src/MemberMethodMain.java
+++ 02.OOP[BASIC]/src/MemberMethodMain.java
@@ -2,7 +2,37 @@
 public class MemberMethodMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		MemberMethod mm=new MemberMethod();
+		mm.number1=11;
+		mm.number2=22;
+		System.out.println("main stmt1");
+		
+		/*
+		 * 멤버메소드 실행(호출)
+		 * - 형식 : 참조(주소)변수.멤버메소드이름();
+		 * - main실행흐름을 mm주소에 있는 객체의
+		 *   method1이라는 이름의 메소드(실행코드가 있는 블록)로 보낸다(호출).
+		 */
+		
+		
+		mm.method1();
+		System.out.println("main stmt2");
+		mm.method2(2);
+		System.out.println("main stmt3");
+		mm.method2(3);
+		System.out.println("main stmt4");
+		mm.method3("안녕하세요", 3);
+		System.out.println("main stmt5");
+		mm.method3("누구세요?", 5);
+		System.out.println("main stmt6");
+		mm.methed4("연승문","잘생겼어요!",3);
+		System.out.println("main stmt7");
+		mm.methed4("이효리","안녕하세요!",5);
+		System.out.println("main stmt8");
+		return;
+		
+		
 
 	}
 

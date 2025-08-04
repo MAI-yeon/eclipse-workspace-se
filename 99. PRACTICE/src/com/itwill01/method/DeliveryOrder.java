4cf3238288a47a8764388ac1968b86b93645402c
연승문 <yeonsm99@naver.com>
2025-08-04 18:23:01 +0900

    ...

---
+++ 99. PRACTICE/src/com/itwill01/method/DeliveryOrder.java
@@ -0,0 +1,13 @@
+package com.itwill01.method;
+
+public class DeliveryOrder {
+	
+	int no;
+	int no2;
+	String juso;
+	int date;
+	int time;
+	int price;
+	int no3;
+
+}
---
+++ 99. PRACTICE/src/com/itwill01/method/DeliveryOrderMain.java
@@ -0,0 +1,16 @@
+package com.itwill01.method;
+
+public class DeliveryOrderMain {
+
+	public static void main(String[] args) {
+		
+		DeliveryOrder deliveryOrder = new DeliveryOrder();
+		
+		deliveryOrder.setOrder(202011020,01023450001,"서울시 강남구 역삼동 111-333",20201102,130258,35000,0003);
+		
+		
+		deliveryOrder.print();
+
+	}
+
+}

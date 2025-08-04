d509d689de2eccc2cda1d7b82c3d9945fc9eb05b
연승문 <yeonsm99@naver.com>
2025-08-04 18:23:07 +0900

    `

--- 99. PRACTICE/src/com/itwill01/method/DeliveryOrder.java
+++ 99. PRACTICE/src/com/itwill01/method/DeliveryOrder.java
@@ -9,5 +9,16 @@ public class DeliveryOrder {
 	int time;
 	int price;
 	int no3;
+	
+	
+	public void setOrder() {
+		System.out.println("주문 접수 번호 : "+no);
+		System.out.println("주문 핸드폰 번호 : "+no2);
+		System.out.println("주문 집 주소 : "+no2);
+		System.out.println("주문 날짜 : "+no2);
+		System.out.println("주문 시간 : "+no2);
+		System.out.println("주문 가격 : "+no2);
+		System.out.println("메뉴 번호 : "+no2);
+	}
 
 }
